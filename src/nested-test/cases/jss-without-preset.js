d6366bafb2ba82a79518fb070ce3a124b16ba271
Philippe Lhoste <PhiLho@GMX.net>
2016-12-14 15:07:26 +0100

    Add nested plugin to jss-without-preset in the nested-test

--- src/nested-test/cases/jss-without-preset.js
+++ src/nested-test/cases/jss-without-preset.js
@@ -1,10 +1,12 @@
 import { create } from 'jss';
+import nested from 'jss-nested';
 import camelCase from 'jss-camel-case';
 import { createStylesheet } from '../styles';
 import { renderHtml, renderBody } from '../render';
 
 export const jssWithoutPresetCase = (caseName) => {
     const jss = create();
+    jss.use(nested());
     jss.use(camelCase());
 
     const options = { prefixPseudo: true };
--- src/simple-test/styles.js
+++ src/simple-test/styles.js
@@ -1,12 +1,16 @@
+// Add units to padding and fontSize,
+// as fela, styletron and jss-witout-preset leave raw numbers as is (not adding px),
+// which is incorrect CSS (Chrome doesn't care, Firefox warns against them).
+// I prefer explicit unit, anyway...
 export const containerStyle = {
     backgroundColor: 'rebeccapurple',
     textAlign: 'center',
-    padding: 50,
+    padding: '50px',
 };
 
 export const buttonStyle = {
     backgroundColor: 'turquoise',
-    fontSize: 30,
+    fontSize: '30px',
     border: '3px solid yellow',
 };
 
